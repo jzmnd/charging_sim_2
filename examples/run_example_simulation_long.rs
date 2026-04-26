99c6126ef50a6e0c5535e45ea7c8303ad1e64fe8
Jeremy Smith <j.smith.03@cantab.net>
2026-04-26 16:26:16 -0500

    Change long sim example parameters

--- examples/run_example_simulation_long.rs
+++ examples/run_example_simulation_long.rs
@@ -17,16 +17,15 @@ fn main() {
 
     let mut vehicles: Vec<Vehicle> = vec![];
 
-    let arrival_sampler = ArrivalSampler::new(
-        [
-            0.0, 0.0, 0.05, 0.05, 0.1, 0.1, 0.15, 0.25, 0.3, 0.35, 0.4, 0.4, 0.35, 0.35, 0.4, 0.45,
-            0.5, 0.55, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
-        ],
-        [0.95, 0.8, 0.8, 0.75, 0.8, 0.85, 1.0],
-        23.4,
-    );
+    let tod_distr = [
+        0.0, 0.0, 0.05, 0.05, 0.1, 0.1, 0.15, 0.25, 0.3, 0.35, 0.4, 0.4, 0.35, 0.35, 0.4, 0.45,
+        0.5, 0.55, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+    ];
+    let dow_distr = [0.95, 0.8, 0.8, 0.75, 0.8, 0.85, 1.0];
+    let avg_sessions_per_day = 23.4;
+    let arrival_sampler = ArrivalSampler::new(tod_distr, dow_distr, avg_sessions_per_day);
 
-    for (i, &time) in arrival_sampler.sample_arrivals(14).iter().enumerate() {
+    for (i, &time) in arrival_sampler.sample_arrivals(365).iter().enumerate() {
         let v = Vehicle::builder()
             .charge_profile_ids(&charge_profile_list.all_ids())
             .charge_profile_weights(&[0.4, 0.6])
@@ -44,11 +43,11 @@ fn main() {
         vec![
             Charger::builder()
                 .max_power_kw(180.0)
-                .max_current_a(100.0)
+                .max_current_a(500.0)
                 .build("c1"),
             Charger::builder()
                 .max_power_kw(180.0)
-                .max_current_a(100.0)
+                .max_current_a(500.0)
                 .build("c2"),
         ],
     );
